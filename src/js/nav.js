68a00a1f369cdaa26263e995f6afdd99959765d0
Precious Tom <tomprezine@gmail.com>
2023-05-10 17:29:37 +0100

    first push

---
+++ src/js/nav.js
@@ -0,0 +1,4 @@
+$("#hamburger").on("click", function(){
+    $("#menu").toggleClass("py-10 opacity-0 -translate-y-twoHundredPercent")
+    $(".hamburger-line").toggleClass('bg-black bg-brandBlue')
+})
